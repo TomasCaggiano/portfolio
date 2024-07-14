9b8e846690b5d45379c951ee38c5fc8bacf0562c
Tomas Caggiano <tomascaggiano03@gmail.com>
2024-07-14 14:56:01 -0300

    primer arreglo

--- src/components/NavBar/NavBar.jsx
+++ src/components/NavBar/NavBar.jsx
@@ -1,23 +1,18 @@
-import Container from 'react-bootstrap/Container';
-import Nav from 'react-bootstrap/Nav';
-import Navbar from 'react-bootstrap/Navbar';
-import { NavLink, Link } from 'react-bootstrap';
-
+import { Container, Nav, Navbar } from 'react-bootstrap';
+import { Link } from 'react-router-dom'; 
 function NavBar() {
-    return (
-      <>
-        <Navbar bg="dark" data-bs-theme="dark">
-          <Container>
-            <Navbar.Brand to="/">Home</Navbar.Brand>
-            <Nav className="me-auto">
-              <Nav.Link to="/contact">Contact</Nav.Link>
-              <Nav.Link to="/Projects">Projects</Nav.Link>
-              <Nav.Link to="/About">About</Nav.Link>
-            </Nav>
-          </Container>
-        </Navbar>
-        </>
-        );
-    }
-    
-    export default NavBar;
+  return (
+    <Navbar bg="dark" variant="dark">
+      <Container>
+        <Navbar.Brand as={Link} to="/">Home</Navbar.Brand>
+        <Nav className="me-auto">
+          <Nav.Link as={Link} to="/contact">Contact</Nav.Link>
+          <Nav.Link as={Link} to="/projects">Projects</Nav.Link>
+          <Nav.Link as={Link} to="/about">About</Nav.Link>
+        </Nav>
+      </Container>
+    </Navbar>
+  );
+}
+
+export default NavBar;
