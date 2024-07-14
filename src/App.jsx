3c3d7041d3fe22de08de2284b26507aa2ebd46b1
Tomas Caggiano <tomascaggiano03@gmail.com>
2024-07-14 14:57:12 -0300

    primer arreglo

--- src/App.jsx
+++ src/App.jsx
@@ -1,28 +1,26 @@
-import { useState } from 'react';
+import React from 'react';
 import './App.css';
-import Products from './components/Products/Products';
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
 import NavBar from './components/NavBar/NavBar';
 import Home from './components/Home/Home';
 import About from './components/About/About';
 import Contact from './components/Contact/Contact';
 import Projects from './components/Projects/Projects';
-import { BrowserRouter, Routes, Route} from "react-router-dom";
 import Footer from './components/Footer/Footer';
 
 function App() {
-  return ( 
+  return (
     <BrowserRouter>
-    <NavBar/>
+      <NavBar />
       <Routes>
-      <Route path='/' element={<Home/>}/>
-      <Route path='/Projects' element={<Projects/>}/>
-      <Route path='/About' element={<About/>}/>
-      <Route path='/Contact' element={<Contact/>}/>
+        <Route path="/" element={<Home />} />
+        <Route path="/projects" element={<Projects />} />
+        <Route path="/about" element={<About />} />
+        <Route path="/contact" element={<Contact />} />
       </Routes>
-      <Footer/>
+      <Footer />
     </BrowserRouter>
-
-  )
+  );
 }
 
-export default App
+export default App;
--- src/components/NavBar/NavBar.jsx
+++ src/components/NavBar/NavBar.jsx
@@ -1,18 +1,22 @@
-import { Container, Nav, Navbar } from 'react-bootstrap';
-import { Link } from 'react-router-dom'; 
-function NavBar() {
-  return (
-    <Navbar bg="dark" variant="dark">
-      <Container>
-        <Navbar.Brand as={Link} to="/">Home</Navbar.Brand>
-        <Nav className="me-auto">
-          <Nav.Link as={Link} to="/contact">Contact</Nav.Link>
-          <Nav.Link as={Link} to="/projects">Projects</Nav.Link>
-          <Nav.Link as={Link} to="/about">About</Nav.Link>
-        </Nav>
-      </Container>
-    </Navbar>
-  );
-}
+import Container from 'react-bootstrap/Container';
+import Nav from 'react-bootstrap/Nav';
+import Navbar from 'react-bootstrap/Navbar';
 
-export default NavBar;
+function NavBar() {
+    return (
+      <>
+        <Navbar bg="dark" data-bs-theme="dark">
+          <Container>
+            <Navbar.Brand href="/">Home</Navbar.Brand>
+            <Nav className="/me-auto">
+              <Nav.Link href="/contact">Contact</Nav.Link>
+              <Nav.Link href="/Projects">Projects</Nav.Link>
+              <Nav.Link href="/About">About</Nav.Link>
+            </Nav>
+          </Container>
+        </Navbar>
+        </>
+        );
+    }
+    
+    export default NavBar;
