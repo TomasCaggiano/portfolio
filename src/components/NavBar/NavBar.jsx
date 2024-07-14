3144a9c6e7a81ae80b5b389d928fbbe8c933423a
Tomas Caggiano <tomascaggiano03@gmail.com>
2024-07-14 14:54:00 -0300

    primer arreglo

--- src/components/NavBar/NavBar.jsx
+++ src/components/NavBar/NavBar.jsx
@@ -1,17 +1,18 @@
 import Container from 'react-bootstrap/Container';
 import Nav from 'react-bootstrap/Nav';
 import Navbar from 'react-bootstrap/Navbar';
+import { NavLink, Link } from 'react-bootstrap';
 
 function NavBar() {
     return (
       <>
         <Navbar bg="dark" data-bs-theme="dark">
           <Container>
-            <Navbar.Brand href="/">Home</Navbar.Brand>
+            <Navbar.Brand to="/">Home</Navbar.Brand>
             <Nav className="me-auto">
-              <Nav.Link href="/contact">Contact</Nav.Link>
-              <Nav.Link href="/Projects">Projects</Nav.Link>
-              <Nav.Link href="/About">About</Nav.Link>
+              <Nav.Link to="/contact">Contact</Nav.Link>
+              <Nav.Link to="/Projects">Projects</Nav.Link>
+              <Nav.Link to="/About">About</Nav.Link>
             </Nav>
           </Container>
         </Navbar>
