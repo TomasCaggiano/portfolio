4bc41d0ff3a2ddf55ab0a12e709a906b6625dec1
Tomas Caggiano <tomascaggiano03@gmail.com>
2024-07-14 21:24:06 -0300

    ultimando detalles

--- src/components/Projects/Projects.jsx
+++ src/components/Projects/Projects.jsx
@@ -8,35 +8,23 @@ const Projects = () => {
       <h1 className='projects-heading'>My Projects</h1>
       <div className='projects-grid'>
         <Products
-          img="https://images.app.goo.gl/bJDMSFd5zU85qXN46"
-          title="Sitio Web 1"
-          desc="Descripción del primer proyecto."
+          img="/images/api.PNG"
+          title="e-commerce-API"
+          desc="Este proyecto de e-commerce desarrollado con React y Bootstrap ofrece una plataforma robusta para compras en línea, enfocándose en una experiencia de usuario fluida y adaptable. Integrado con una API para la gestión dinámica de productos, 
+          el sitio permite a los usuarios buscar, filtrar y agregar artículos al carrito de manera intuitiva. El diseño responsive asegura una navegación consistente en dispositivos móviles y de escritorio, mientras que la configuración optimizada con Vite facilita un despliegue eficiente en GitHub Pages. 
+          Con funcionalidades esenciales como la gestión de carritos y un proceso de compra simulado, este proyecto no solo exhibe competencia en desarrollo front-end, sino también la capacidad para implementar soluciones escalables y adaptativas para negocios de e-commerce."
           link="Ver más"
           url="https://www.example.com"
         />
 
         <Products
-          img="https://images.app.goo.gl/bJDMSFd5zU85qXN46"
-          title="Sitio Web 2"
-          desc="Descripción del segundo proyecto."
+          img="/images/firebase.PNG"
+          title="e-commerce-firebase"
+          desc="Este proyecto de e-commerce implementado con Firebase y React ofrece una solución robusta para la gestión y visualización de productos en línea. Utilizando Firestore para el almacenamiento de datos y Firebase Authentication para la autenticación de usuarios,
+          el sitio proporciona una experiencia segura y personalizada. El diseño responsive garantiza una navegación fluida en dispositivos móviles y de escritorio, mientras que la integración con Firebase Hosting asegura un despliegue eficiente y escalable.
+          Funcionalidades clave incluyen la gestión dinámica de productos, carrito de compras persistente y un flujo de compra completo, demostrando habilidades avanzadas en el desarrollo front-end y la implementación de soluciones en la nu"
           link="Ver más"
-          url="https://www.example.com"
-        />
-
-        <Products
-          img="https://images.app.goo.gl/bJDMSFd5zU85qXN46"
-          title="Sitio Web 3"
-          desc="Descripción del tercer proyecto."
-          link="Ver más"
-          url="https://www.example.com"
-        />
-
-        <Products
-          img="https://images.app.goo.gl/bJDMSFd5zU85qXN46"
-          title="Sitio Web 4"
-          desc="Descripción del cuarto proyecto."
-          link="Ver más"
-          url="https://www.example.com"
+          url="https://e-commerce-firebase1.netlify.app/"
         />
       </div>
     </div>
